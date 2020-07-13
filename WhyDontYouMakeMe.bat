9503ae03b042a657026a85032d218ac51952b2f8
MythicOrange <gpeklaj@gmail.com>
2020-07-13 10:19:24 +0200

    Stability improvement x10
    Updated functionalities to check for existing structures, the utility can now be safely ran to self- "install". Utility will now also work properly if ran more than once per day. More fixes to come, specifically language changes and template file change.

--- WhyDontYouMakeMe.bat
+++ WhyDontYouMakeMe.bat
@@ -1,14 +1,21 @@
-::@ECHO OFF
+GOTO SCRIPTSET
+:SCRIPTSET
 SET "scriptdir=%~dp0"
 IF NOT "%scriptdir:~-1%"=="\" SET "scriptdir=%scriptdir%\"
-IF NOT EXSISTS ************ GOTO MAKEME
-
+GOTO CHECKME
+:CHECKME
+IF EXIST "%scriptdir%Zapisniki GOTO DATEME
+mkdir Zapisniki 
+attrib WhyDontYouMakeMe.bat +h
+attrib NePremikiNeTiki.odt +h
+mklink "%scriptdir%WhyDontYouMakeMe.bat" "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup\WhyDontYouMakeMe.bat"
+:DATEME
+IF EXIST "%scriptdir%Zapisniki\%date:~-7,2%-%date:~0,2%-%date:~-4,4%" GOTO MAKEME
+mkdir "%scriptdir%Zapisniki\%date:~-7,2%-%date:~0,2%-%date:~-4,4%"
 :MAKEME
-mkdir %date:~-7,2%-%date:~0,2%-%date:~-4,4%
-**prestavi na konec** %SystemRoot%\explorer.exe "%scriptdir%%date:~-7,2%-%date:~0,2%-%date:~-4,4%"
-xcopy "%scriptdir%NePremikiNeTiki.odt" "%scriptdir%%date:~-7,2%-%date:~0,2%-%date:~-4,4%\"
-SKEČI*****RENAME "%scriptdir%%date:~-7,2%-%date:~0,2%-%date:~-4,4%\NePremikiNeTiki.odt" "%date:~-7,2%-%date:~0,2%-%date:~-4,4%_Zapisnik.odt"
+IF EXIST "%scriptdir%Zapisniki\%date:~-7,2%-%date:~0,2%-%date:~-4,4%\%date:~-7,2%-%date:~0,2%-%date:~-4,4%.odt" GOTO OPENME
+xcopy "%scriptdir%NePremikiNeTiki.odt" "%scriptdir%Zapisniki\%date:~-7,2%-%date:~0,2%-%date:~-4,4%\"
+RENAME "%scriptdir%Zapisniki\%date:~-7,2%-%date:~0,2%-%date:~-4,4%\NePremikiNeTiki.odt" "%date:~-7,2%-%date:~0,2%-%date:~-4,4%_Zapisnik.odt"
+:OPENME
+%SystemRoot%\explorer.exe "%scriptdir%Zapisniki\%date:~-7,2%-%date:~0,2%-%date:~-4,4%"
 EXIT
-
-
-%AppData%\Microsoft\Windows\Start Menu\Programs\Startup
